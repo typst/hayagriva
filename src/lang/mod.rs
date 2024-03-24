c193bbfbb2a47998c44ca69f75cc53590c4bba4a
Nikita Rydanov <find.art.in.living@gmail.com>
2024-03-24 17:57:27 +0400

    Removed language codes

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -3,9 +3,6 @@
 pub(crate) mod en;
 pub(crate) mod name;
 
-/// This modules contains functions to use locale codes
-pub mod codes;
-
 use std::{fmt::Write, mem};
 
 use crate::types::{FoldableKind, FoldableStringChunk};
