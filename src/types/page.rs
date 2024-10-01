typst/hayagriva

commit 8f3e1830fa035f2ec4d639c90ae383cfb3a00ea3

Parse page ranges following citeproc (#155)
Co-authored-by: Martin Haug <mhaug@live.de>
Co-authored-by: Daniel Drodt <132357467+Drodt@users.noreply.github.com>

src/types/page.rs
@@ -1,0 +1,338 @@
+use std::{cmp::Ordering, fmt::Display, num::NonZeroUsize, str::FromStr};
+
+use crate::{MaybeTyped, Numeric, NumericError};
+
+use super::{deserialize_from_str, serialize_display};
+use serde::{de, Deserialize, Serialize};
+use thiserror::Error;
+
+impl MaybeTyped<PageRanges> {
+    /// Order the values according to CSL rules.
+    pub(crate) fn csl_cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match (self, other) {
+            (MaybeTyped::Typed(a), MaybeTyped::Typed(b)) => a.csl_cmp(b),
+            _ => self.to_string().cmp(&other.to_string()),
+        }
+    }
+}
+
+/// Ranges of page numbers, e.g., `1-4, 5 & 6`.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct PageRanges {
+    /// The given ranges.
+    pub ranges: Vec<PageRangesPart>,
+}
+
+impl PageRanges {
+    /// Create a new `PageRanges` struct.
+    pub fn new(ranges: Vec<PageRangesPart>) -> Self {
+        Self { ranges }
+    }
+
+    /// Get the first page of the first range.
+    pub fn first(&self) -> Option<&Numeric> {
+        self.ranges.first().and_then(PageRangesPart::start)
+    }
+
+    /// Order the values according to CSL rules.
+    pub(crate) fn csl_cmp(&self, other: &Self) -> std::cmp::Ordering {
+        #[derive(PartialEq, Eq)]
+        struct OrderablePageRangesPart<'a>(&'a PageRangesPart);
+
+        impl Ord for OrderablePageRangesPart<'_> {
+            fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+                self.0.csl_cmp(other.0)
+            }
+        }
+
+        impl PartialOrd for OrderablePageRangesPart<'_> {
+            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+                Some(self.cmp(other))
+            }
+        }
+
+        self.ranges
+            .iter()
+            .map(OrderablePageRangesPart)
+            .cmp(other.ranges.iter().map(OrderablePageRangesPart))
+    }
+
+    /// Whether to pluralize the `pages` term, when used with this page range.
+    pub fn is_plural(&self) -> bool {
+        self.ranges.len() != 1
+    }
+}
+
+impl From<u64> for PageRanges {
+    fn from(value: u64) -> Self {
+        Self { ranges: vec![value.into()] }
+    }
+}
+
+impl Display for PageRanges {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.ranges.iter().try_for_each(|r| r.fmt(f))
+    }
+}
+
+impl FromStr for PageRanges {
+    type Err = PageRangesPartErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // Split input into different ranges separated by `&` or `,`
+        Ok(Self {
+            ranges: group_by(s, |c, d| !(c == ',' || c == '&' || d == ',' || d == '&'))
+                .map(PageRangesPart::from_str)
+                .collect::<Result<_, _>>()?,
+        })
+    }
+}
+
+/// Parts of the page ranges.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum PageRangesPart {
+    /// An and, i.e, `&`.
+    Ampersand,
+    /// A comma, i.e., `,`.
+    Comma,
+    /// An escaped range with start and end, e.g., `1\-4`.
+    EscapedRange(Numeric, Numeric),
+    /// A single page, e.g., `5`.
+    SinglePage(Numeric),
+    /// A full range, e.g., `1n8--1n14`.
+    Range(Numeric, Numeric),
+}
+
+impl PageRangesPart {
+    /// The start of a range if any.
+    pub fn start(&self) -> Option<&Numeric> {
+        match self {
+            Self::EscapedRange(s, _) => Some(s),
+            Self::SinglePage(s) => Some(s),
+            Self::Range(s, _) => Some(s),
+            _ => None,
+        }
+    }
+
+    /// Order the values according to CSL rules.
+    pub(crate) fn csl_cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match (self, other) {
+            (Self::Ampersand, Self::Ampersand) => Ordering::Equal,
+            (Self::Ampersand, _) => Ordering::Less,
+            (_, Self::Ampersand) => Ordering::Greater,
+            (Self::Comma, Self::Comma) => Ordering::Equal,
+            (Self::Comma, _) => Ordering::Less,
+            (_, Self::Comma) => Ordering::Greater,
+            (Self::SinglePage(n1), Self::SinglePage(n2)) => n1.csl_cmp(n2),
+            (Self::SinglePage(_), _) => Ordering::Less,
+            (_, Self::SinglePage(_)) => Ordering::Greater,
+            (Self::EscapedRange(s1, e1), Self::EscapedRange(s2, e2)) => {
+                let ord = s1.csl_cmp(s2);
+                if ord != Ordering::Equal {
+                    return ord;
+                }
+                e1.csl_cmp(e2)
+            }
+            (Self::EscapedRange(_, _), _) => Ordering::Less,
+            (_, Self::EscapedRange(_, _)) => Ordering::Greater,
+            (Self::Range(s1, e1), Self::Range(s2, e2)) => {
+                let ord = s1.csl_cmp(s2);
+                if ord != Ordering::Equal {
+                    return ord;
+                }
+                e1.csl_cmp(e2)
+            }
+        }
+    }
+}
+
+impl From<u64> for PageRangesPart {
+    fn from(value: u64) -> Self {
+        Self::SinglePage((value as u32).into())
+    }
+}
+
+impl Display for PageRangesPart {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            PageRangesPart::Ampersand => "&",
+            PageRangesPart::Comma => ", ",
+            PageRangesPart::EscapedRange(s, e) => return write!(f, "{s}-{e}"),
+            PageRangesPart::SinglePage(s) => return write!(f, "{s}"),
+            PageRangesPart::Range(s, e) => return write!(f, "{s}-{e}"),
+        };
+        Display::fmt(s, f)
+    }
+}
+
+/// Parsing error for page ranges.
+#[derive(Debug, Clone, Copy, Error)]
+pub enum PageRangesPartErr {
+    /// The string is malformed.
+    #[error("page range string malformed")]
+    Malformed,
+    /// The string is empty.
+    #[error("page range is empty")]
+    Empty,
+    /// An error from parsing a numeric value.
+    #[error("page range contained invalid numeric value")]
+    NumericErr(#[from] NumericError),
+}
+
+impl FromStr for PageRangesPart {
+    type Err = PageRangesPartErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+        if s.is_empty() {
+            return Err(PageRangesPartErr::Empty);
+        }
+        let p = if s == "&" {
+            Self::Ampersand
+        } else if s == "," {
+            Self::Comma
+        } else if s.contains("\\-") {
+            // If `-` chars are escaped, write `-`.
+            let mut parts = s.split("\\-").map(str::trim);
+
+            let start = parts.next().ok_or(PageRangesPartErr::Empty)?;
+            let end = parts.next().ok_or(PageRangesPartErr::Empty)?;
+
+            let r = Self::EscapedRange(parse_number(start)?, parse_number(end)?);
+            if parts.next().is_some() {
+                return Err(PageRangesPartErr::Malformed);
+            }
+            r
+        } else {
+            // Otherwise, split into the two halves of the dash.
+            let mut parts = s.split(['-', '–']).map(str::trim);
+            let r = match (parts.next(), parts.next()) {
+                (None, None) => unreachable!(),
+                (Some(start), None) => Self::SinglePage(parse_number(start)?),
+                (Some(start), Some(end)) => {
+                    Self::Range(parse_number(start)?, parse_number(end)?)
+                }
+                _ => unreachable!(),
+            };
+            if parts.next().is_some() {
+                return Err(PageRangesPartErr::Malformed);
+            }
+            r
+        };
+        Ok(p)
+    }
+}
+
+deserialize_from_str!(PageRanges);
+serialize_display!(PageRanges);
+
+fn parse_number(s: &str) -> Result<Numeric, NumericError> {
+    Numeric::from_str(s)
+}
+
+/// Split `s` into maximal chunks such that two successive chars satisfy `pred`.
+///
+/// Returns an iterator over these chunks.
+pub(crate) fn group_by<F>(s: &str, pred: F) -> GroupBy<'_, F>
+where
+    F: FnMut(char, char) -> bool,
+{
+    GroupBy::new(s, pred)
+}
+
+/// An iterator over string slice in (non-overlapping) chunks separated by a predicate.
+///
+/// Adapted from the nightly std.
+pub(crate) struct GroupBy<'a, P> {
+    string: &'a str,
+    predicate: P,
+}
+
+impl<'a, P> GroupBy<'a, P> {
+    pub(crate) fn new(string: &'a str, predicate: P) -> Self {
+        GroupBy { string, predicate }
+    }
+}
+
+impl<'a, P> Iterator for GroupBy<'a, P>
+where
+    P: FnMut(char, char) -> bool,
+{
+    type Item = &'a str;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.string.is_empty() {
+            None
+        } else {
+            let mut len = 1;
+            for w in windows(self.string, 2) {
+                let chars: Vec<_> = w.chars().collect();
+                let (c, d) = (chars[0], chars[1]);
+                if (self.predicate)(c, d) {
+                    len += c.len_utf8();
+                } else {
+                    break;
+                }
+            }
+            let (head, tail) = self.string.split_at(len);
+            self.string = tail;
+            Some(head)
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.string.chars().size_hint()
+    }
+}
+
+/// Return an iterator of sliding windows of size `size` over `string`.
+///
+/// # Panic
+///
+/// Panics if `size` is zero.
+pub(crate) fn windows(string: &str, size: usize) -> Windows<'_> {
+    assert!(size > 0);
+    Windows::new(string, NonZeroUsize::new(size).unwrap())
+}
+
+/// An iterator of sliding windows of size `size` over `string`.
+///
+/// Each call of `next` advanced the window by one.
+pub(crate) struct Windows<'a> {
+    string: &'a str,
+    size: NonZeroUsize,
+}
+
+impl<'a> Windows<'a> {
+    pub(crate) fn new(string: &'a str, size: NonZeroUsize) -> Self {
+        Self { string, size }
+    }
+}
+
+impl<'a> Iterator for Windows<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let size = self.size.get();
+        if size > self.string.len() {
+            None
+        } else {
+            let mut indices = self.string.char_indices();
+            let next = indices.nth(1).unwrap().0;
+            match indices.nth(size - 2) {
+                Some((idx, _)) => {
+                    let ret = Some(&self.string[..idx]);
+                    self.string = &self.string[next..];
+                    ret
+                }
+                None => {
+                    let ret = Some(self.string);
+                    self.string = "";
+                    ret
+                }
+            }
+        }
+    }
+}

Cargo.toml
@@ -17,7 +17,7 @@ archive = ["ciborium"]
 csl-json = ["citationberg/json"]
 
 [dependencies]
-citationberg = { git = "https://github.com/typst/citationberg.git", rev = "61ca6a7fcc48365f805e521cc8bc1f8f679ff372" }
+citationberg = { git = "https://github.com/typst/citationberg.git", rev = "e3fd3f08e0e16983b7c3514b791b64c704dc2524" }
 indexmap = { version = "2.0.2", features = ["serde"] }
 numerals = "0.1.4"
 paste = "1.0.14"

src/csl/mod.rs
@@ -8,8 +8,8 @@ use std::num::{NonZeroI16, NonZeroUsize};
 use std::{mem, vec};
 
 use citationberg::taxonomy::{
-    DateVariable, Locator, NameVariable, NumberVariable, OtherTerm, StandardVariable,
-    Term, Variable,
+    DateVariable, Locator, NameVariable, NumberVariable, OtherTerm, PageVariable,
+    StandardVariable, Term, Variable,
 };
 use citationberg::{
     taxonomy as csl_taxonomy, Affixes, BaseLanguage, Citation, CitationFormat, Collapse,
@@ -30,7 +30,7 @@ use self::elem::last_text_mut_child;
 pub use self::elem::{
     BufWriteFormat, Elem, ElemChild, ElemChildren, ElemMeta, Formatted, Formatting,
 };
-use self::taxonomy::{EntryLike, NumberVariableResult};
+use self::taxonomy::{EntryLike, NumberVariableResult, PageVariableResult};
 
 #[cfg(feature = "archive")]
 pub mod archive;
@@ -2565,6 +2565,14 @@ impl<'a, T: EntryLike> Context<'a, T> {
         self.writing.prepare_variable_query(variable)?;
         let res = self.instance.resolve_number_variable(variable);
         res
+    }
+
+    fn resolve_page_variable(
+        &self,
+        variable: PageVariable,
+    ) -> Option<PageVariableResult> {
+        self.writing.prepare_variable_query(variable)?;
+        self.instance.resolve_page_variable(variable)
     }
 
     /// Resolve a name variable.

src/csl/rendering/mod.rs
@@ -3,18 +3,21 @@ use std::fmt::Write;
 use std::str::FromStr;
 
 use citationberg::taxonomy::{
-    Locator, NumberVariable, OtherTerm, StandardVariable, Term, Variable,
+    Locator, NumberOrPageVariable, NumberVariable, OtherTerm, PageVariable,
+    StandardVariable, Term, Variable,
 };
 use citationberg::{
     ChooseBranch, CslMacro, DateDayForm, DateMonthForm, DatePartName, DateParts,
     DateStrongAnyForm, GrammarGender, LabelPluralize, LayoutRenderingElement,
-    LongShortForm, NumberForm, TestPosition, TextCase, ToAffixes, ToFormatting,
+    LongShortForm, NumberForm, PageRangeFormat, TestPosition, TextCase, ToAffixes,
+    ToFormatting,
 };
 use citationberg::{TermForm, TextTarget};
 
-use crate::csl::taxonomy::NumberVariableResult;
+use crate::csl::taxonomy::{NumberVariableResult, PageVariableResult};
 use crate::lang::{Case, SentenceCase, TitleCase};
 use crate::types::{ChunkedString, Date, MaybeTyped, Numeric};
+use crate::PageRanges;
 
 use super::taxonomy::EntryLike;
 use super::{Context, ElemMeta, IbidState, SpecialForm, UsageInfo};
@@ -81,20 +84,18 @@ impl RenderCsl for citationberg::Text {
                 }
                 _ => ctx.push_chunked(&val),
             },
-            ResolvedTextTarget::NumberVariable(var, n) => match n {
+            ResolvedTextTarget::NumberVariable(_, n) => match n {
                 NumberVariableResult::Regular(MaybeTyped::Typed(num))
                     if num.will_transform() =>
                 {
-                    render_typed_num(num.as_ref(), NumberForm::default(), var, None, ctx);
-                }
-                NumberVariableResult::Regular(n)
-                    if matches!(var, NumberVariable::Page) =>
-                {
-                    // TODO: Remove this hack
-                    ctx.push_str(&n.to_str().replace('-', "–"))
+                    render_typed_num(num.as_ref(), NumberForm::default(), None, ctx);
                 }
                 NumberVariableResult::Regular(n) => ctx.push_str(&n.to_str()),
                 NumberVariableResult::Transparent(n) => ctx.push_transparent(n),
+            },
+            ResolvedTextTarget::PageVariable(p) => match p {
+                MaybeTyped::Typed(r) => render_page_range(&r, ctx),
+                MaybeTyped::String(s) => ctx.push_str(&s.replace('-', "–")),
             },
             ResolvedTextTarget::Macro(mac) => {
                 for child in &mac.children {
@@ -138,6 +139,9 @@ impl RenderCsl for citationberg::Text {
         match target {
             ResolvedTextTarget::StandardVariable(s, _) => var == Variable::Standard(s),
             ResolvedTextTarget::NumberVariable(n, _) => var == Variable::Number(n),
+            ResolvedTextTarget::PageVariable(_) => {
+                var == Variable::Page(PageVariable::Page)
+            }
             ResolvedTextTarget::Macro(mac) => {
                 mac.children.iter().any(|c| c.will_render(ctx, var))
             }
@@ -170,7 +174,8 @@ impl RenderCsl for citationberg::Text {
                 (false, UsageInfo { has_vars: true, ..Default::default() })
             }
             ResolvedTextTarget::StandardVariable(_, _)
-            | ResolvedTextTarget::NumberVariable(_, _) => (
+            | ResolvedTextTarget::NumberVariable(_, _)
+            | ResolvedTextTarget::PageVariable(_) => (
                 true,
                 UsageInfo {
                     has_vars: true,
@@ -198,6 +203,7 @@ impl RenderCsl for citationberg::Text {
 enum ResolvedTextTarget<'a, 'b> {
     StandardVariable(StandardVariable, Cow<'a, ChunkedString>),
     NumberVariable(NumberVariable, NumberVariableResult<'a>),
+    PageVariable(PageVariableResult),
     Macro(&'a CslMacro),
     Term(&'a str),
     Value(&'b str),
@@ -243,6 +249,9 @@ impl<'a, 'b> ResolvedTextTarget<'a, 'b> {
             TextTarget::Variable { var: Variable::Number(var), .. } => ctx
                 .resolve_number_variable(*var)
                 .map(|n| ResolvedTextTarget::NumberVariable(*var, n)),
+            TextTarget::Variable { var: Variable::Page(pv), .. } => {
+                ctx.resolve_page_variable(*pv).map(ResolvedTextTarget::PageVariable)
+            }
             TextTarget::Variable { .. } => None,
             TextTarget::Macro { name } => {
                 ctx.style.get_macro(name).map(ResolvedTextTarget::Macro)
@@ -278,7 +287,7 @@ impl RenderCsl for citationberg::Number {
             Some(NumberVariableResult::Regular(MaybeTyped::Typed(num)))
                 if num.will_transform() =>
             {
-                render_typed_num(num.as_ref(), self.form, self.variable, gender, ctx);
+                render_typed_num(num.as_ref(), self.form, gender, ctx);
             }
             Some(NumberVariableResult::Regular(MaybeTyped::Typed(num))) => {
                 write!(ctx, "{}", num).unwrap()
@@ -345,41 +354,31 @@ impl RenderCsl for citationberg::Number {
 fn render_typed_num<T: EntryLike>(
     num: &Numeric,
     form: NumberForm,
-    variable: NumberVariable,
     gender: Option<GrammarGender>,
     ctx: &mut Context<T>,
 ) {
-    let normal_num = if form == NumberForm::Numeric && variable == NumberVariable::Page {
-        if let Some(range) = num.range() {
-            render_page_range(range, ctx);
-            false
-        } else {
-            true
-        }
-    } else {
-        true
-    };
-
-    if normal_num {
-        num.with_form(ctx, form, gender, ctx.ordinal_lookup()).unwrap();
-    }
+    num.with_form(ctx, form, gender, &ctx.ordinal_lookup()).unwrap();
 }
 
-fn render_page_range<T: EntryLike>(
-    range: std::ops::RangeInclusive<i32>,
-    ctx: &mut Context<T>,
-) {
-    ctx.style
-        .csl
-        .settings
-        .page_range_format
-        .unwrap_or_default()
-        .format(
-            range,
-            ctx,
-            ctx.term(OtherTerm::PageRangeDelimiter.into(), TermForm::default(), false)
-                .or(Some("–")),
-        )
+fn render_page_range<T: EntryLike>(range: &PageRanges, ctx: &mut Context<T>) {
+    let format = ctx.style.csl.settings.page_range_format.unwrap_or_default();
+    let delim = ctx
+        .term(OtherTerm::PageRangeDelimiter.into(), TermForm::default(), false)
+        .or(Some("–"));
+
+    range
+        .ranges
+        .iter()
+        .try_for_each(|r| match r {
+            crate::PageRangesPart::Ampersand => ctx.write_str(" & "),
+            crate::PageRangesPart::Comma => ctx.write_str(", "),
+            crate::PageRangesPart::EscapedRange(start, end) => PageRangeFormat::Expanded
+                .format(ctx, &start.to_string(), &end.to_string(), delim),
+            crate::PageRangesPart::SinglePage(page) => ctx.write_str(&page.to_string()),
+            crate::PageRangesPart::Range(start, end) => {
+                format.format(ctx, &start.to_string(), &end.to_string(), delim)
+            }
+        })
         .unwrap();
 }
 
@@ -393,7 +392,11 @@ fn label_pluralization(
         LabelPluralize::Contextual => match variable {
             NumberVariableResult::Regular(MaybeTyped::String(_)) => false,
             NumberVariableResult::Regular(MaybeTyped::Typed(n)) => {
-                n.is_plural(label.variable.is_number_of_variable())
+                if let NumberOrPageVariable::Number(v) = label.variable {
+                    n.is_plural(v.is_number_of_variable())
+                } else {
+                    panic!("Incompatiable variable types")
+                }
             }
             NumberVariableResult::Transparent(_) => false,
         },
@@ -406,19 +409,40 @@ impl RenderCsl for citationberg::Label {
             return;
         }
 
-        let Some(variable) = ctx.resolve_number_variable(self.variable) else {
-            return;
-        };
+        match self.variable {
+            NumberOrPageVariable::Number(n) => {
+                let Some(variable) = ctx.resolve_number_variable(n) else {
+                    return;
+                };
 
-        let depth = ctx.push_elem(citationberg::Formatting::default());
-        let plural = label_pluralization(self, variable);
+                let depth = ctx.push_elem(citationberg::Formatting::default());
+                let plural = label_pluralization(self, variable);
 
-        let content = ctx
-            .term(Term::from(self.variable), self.label.form, plural)
-            .unwrap_or_default();
+                let content = ctx
+                    .term(Term::from(self.variable), self.label.form, plural)
+                    .unwrap_or_default();
 
-        render_label_with_var(&self.label, ctx, content);
-        ctx.commit_elem(depth, None, Some(ElemMeta::Label));
+                render_label_with_var(&self.label, ctx, content);
+                ctx.commit_elem(depth, None, Some(ElemMeta::Label));
+            }
+            NumberOrPageVariable::Page(pv) => {
+                let Some(p) = ctx.resolve_page_variable(pv) else {
+                    return;
+                };
+
+                let depth = ctx.push_elem(citationberg::Formatting::default());
+                let plural = match p {
+                    MaybeTyped::Typed(p) => p.is_plural(),
+                    _ => false,
+                };
+
+                let content =
+                    ctx.term(Term::from(pv), self.label.form, plural).unwrap_or_default();
+
+                render_label_with_var(&self.label, ctx, content);
+                ctx.commit_elem(depth, None, Some(ElemMeta::Label));
+            }
+        }
     }
 
     fn will_render<T: EntryLike>(&self, _ctx: &mut Context<T>, _var: Variable) -> bool {
@@ -427,7 +451,9 @@ impl RenderCsl for citationberg::Label {
 
     fn will_have_info<T: EntryLike>(&self, ctx: &mut Context<T>) -> (bool, UsageInfo) {
         match ctx.instance.kind {
-            Some(SpecialForm::VarOnly(Variable::Number(n))) if self.variable != n => {
+            Some(SpecialForm::VarOnly(Variable::Number(n)))
+                if self.variable != NumberOrPageVariable::Number(n) =>
+            {
                 return (false, UsageInfo::default());
             }
             Some(
@@ -435,7 +461,8 @@ impl RenderCsl for citationberg::Label {
                 | SpecialForm::OnlyFirstDate
                 | SpecialForm::OnlyYearSuffix,
             ) => {
-                if self.variable != NumberVariable::Locator {
+                if self.variable != NumberOrPageVariable::Number(NumberVariable::Locator)
+                {
                     return (true, UsageInfo::default());
                 }
             }
@@ -443,7 +470,7 @@ impl RenderCsl for citationberg::Label {
         }
 
         // Never yield a label if the locator is set to custom.
-        if self.variable == NumberVariable::Locator
+        if self.variable == NumberOrPageVariable::Number(NumberVariable::Locator)
             && ctx
                 .instance
                 .cite_props
@@ -454,14 +481,33 @@ impl RenderCsl for citationberg::Label {
             return (false, UsageInfo::default());
         }
 
-        if let Some(num) = ctx.resolve_number_variable(self.variable) {
-            let plural = label_pluralization(self, num);
-            (
-                ctx.term(Term::from(self.variable), self.label.form, plural).is_some(),
-                UsageInfo::default(),
-            )
-        } else {
-            (false, UsageInfo::default())
+        match self.variable {
+            NumberOrPageVariable::Number(n) => {
+                if let Some(num) = ctx.resolve_number_variable(n) {
+                    let plural = label_pluralization(self, num);
+                    (
+                        ctx.term(Term::from(self.variable), self.label.form, plural)
+                            .is_some(),
+                        UsageInfo::default(),
+                    )
+                } else {
+                    (false, UsageInfo::default())
+                }
+            }
+            NumberOrPageVariable::Page(pv) => {
+                if let Some(p) = ctx.resolve_page_variable(pv) {
+                    let plural = match p {
+                        MaybeTyped::Typed(p) => p.is_plural(),
+                        _ => false,
+                    };
+                    (
+                        ctx.term(Term::from(pv), self.label.form, plural).is_some(),
+                        UsageInfo::default(),
+                    )
+                } else {
+                    (false, UsageInfo::default())
+                }
+            }
         }
     }
 }
@@ -1090,6 +1136,9 @@ impl<'a, 'b, T: EntryLike> Iterator for BranchConditionIter<'a, 'b, T> {
                         Variable::Date(d) => self.ctx.resolve_date_variable(d).is_some(),
                         Variable::Name(n) => {
                             !self.ctx.resolve_name_variable(n).is_empty()
+                        }
+                        Variable::Page(pv) => {
+                            self.ctx.resolve_page_variable(pv).is_some()
                         }
                     })
                 } else {

src/csl/sort.rs
@@ -82,6 +82,19 @@ impl<'a> StyleContext<'a> {
                     (None, None) => Ordering::Equal,
                 }
             }
+            SortKey::Variable { variable: Variable::Page(pv), .. } => {
+                let a =
+                    InstanceContext::sort_instance(a, a_idx).resolve_page_variable(*pv);
+                let b =
+                    InstanceContext::sort_instance(b, b_idx).resolve_page_variable(*pv);
+
+                match (a, b) {
+                    (Some(a), Some(b)) => a.csl_cmp(&b),
+                    (Some(_), None) => Ordering::Greater,
+                    (None, Some(_)) => Ordering::Less,
+                    (None, None) => Ordering::Equal,
+                }
+            }
             SortKey::MacroName {
                 name,
                 names_min,

commit 2f8a083b7ee74093fe7aa1bce292023800e02e6b

Fix regression introduced in #155

src/types/page.rs
@@ -31,7 +31,7 @@ impl PageRanges {
 
     /// Get the first page of the first range.
     pub fn first(&self) -> Option<&Numeric> {
-        self.ranges.first().and_then(PageRangesPart::start)
+        self.ranges.iter().find_map(PageRangesPart::start)
     }
 
     /// Order the values according to CSL rules.
@@ -59,7 +59,20 @@ impl PageRanges {
 
     /// Whether to pluralize the `pages` term, when used with this page range.
     pub fn is_plural(&self) -> bool {
-        self.ranges.len() != 1
+        let mut count = 0;
+        for range in &self.ranges {
+            match range {
+                PageRangesPart::SinglePage(_) => count += 1,
+                PageRangesPart::Range(s, e) | PageRangesPart::EscapedRange(s, e) => {
+                    if s != e {
+                        return true;
+                    }
+                    count += 1
+                }
+                _ => {}
+            }
+        }
+        count > 1
     }
 }
 
@@ -104,12 +117,22 @@ pub enum PageRangesPart {
 }
 
 impl PageRangesPart {
-    /// The start of a range if any.
+    /// The start of a range, if any.
     pub fn start(&self) -> Option<&Numeric> {
         match self {
             Self::EscapedRange(s, _) => Some(s),
             Self::SinglePage(s) => Some(s),
             Self::Range(s, _) => Some(s),
+            _ => None,
+        }
+    }
+
+    /// The end of a range, if any.
+    pub fn end(&self) -> Option<&Numeric> {
+        match self {
+            Self::EscapedRange(_, e) => Some(e),
+            Self::Range(_, e) => Some(e),
+            Self::SinglePage(_) => None,
             _ => None,
         }
     }