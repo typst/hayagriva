typst/hayagriva

commit 05f95938d2433eeb1550f42af0c5921897758b96

Fix not using short titles (#174)
Co-authored-by: PJTSearch <pjtsignups@gmail.com>

src/interop.rs
@@ -279,8 +279,14 @@ impl TryFrom<&tex::Entry> for Entry {
             item.add_affiliated_persons((a, PersonRole::Introduction));
         }
 
-        if let Some(title) = map_res(entry.title())?.map(Into::into) {
-            item.set_title(title);
+        if let Some(title) = map_res(entry.title())?.map(Into::<ChunkedString>::into) {
+            if let Some(short_title) =
+                map_res(entry.short_title())?.map(Into::<ChunkedString>::into)
+            {
+                item.set_title(FormatString::with_short(title, short_title));
+            } else {
+                item.set_title(FormatString::with_value(title));
+            }
         }
 
         // NOTE: Ignoring subtitle and titleaddon for now

src/csl/taxonomy.rs
@@ -347,13 +347,13 @@ impl EntryLike for Entry {
                 .map(|f| f.select(form))
                 .map(Cow::Borrowed),
             StandardVariable::Status => None,
-            StandardVariable::Title => entry
+            StandardVariable::Title => {
+                entry.title().map(|f| f.select(form)).map(Cow::Borrowed)
+            }
+            StandardVariable::TitleShort => entry
                 .title()
                 .map(|f| f.select(LongShortForm::Short))
                 .map(Cow::Borrowed),
-            StandardVariable::TitleShort => {
-                entry.title().map(|f| f.select(form)).map(Cow::Borrowed)
-            }
             StandardVariable::URL => entry
                 .map(|e| e.url())
                 .map(|d| Cow::Owned(StringChunk::verbatim(d.to_string()).into())),

src/types/strings.rs
@@ -251,6 +251,12 @@ impl Serialize for ChunkedString {
     }
 }
 
+impl Into<String> for ChunkedString {
+    fn into(self) -> String {
+        return self.to_string();
+    }
+}
+
 impl ChunkedString {
     /// Creates a new empty `ChunkedString`.
     pub fn new() -> Self {

commit d9c7a1e2addcadea871b8dcbf4de00c35f775685

Fix regression introduced in #174

src/interop.rs
@@ -279,13 +279,14 @@ impl TryFrom<&tex::Entry> for Entry {
             item.add_affiliated_persons((a, PersonRole::Introduction));
         }
 
-        if let Some(title) = map_res(entry.title())?.map(Into::<ChunkedString>::into) {
-            if let Some(short_title) =
-                map_res(entry.short_title())?.map(Into::<ChunkedString>::into)
-            {
-                item.set_title(FormatString::with_short(title, short_title));
+        if let Some(title) = map_res(entry.title())?.map(Into::into) {
+            if let Some(short_title) = map_res(entry.short_title())?.map(Into::into) {
+                item.set_title(FormatString {
+                    value: title,
+                    short: Some(Box::new(short_title)),
+                });
             } else {
-                item.set_title(FormatString::with_value(title));
+                item.set_title(FormatString { value: title, short: None });
             }
         }
 

src/lib.rs
@@ -63,7 +63,7 @@ let result = driver.finish(BibliographyRequest {
 });
 
 for cite in result.citations {
-    println!("{}", cite.citation.to_string())
+    println!("{}", cite.citation)
 }
 ```
 