d772c7cfb6610b73c773fbfa2a61286c4cba1f6f
DerDrodt <d.drodt@gmx.de>
2024-10-17 09:19:13 +0200

    Fix bib layout affixes

--- tests/citeproc.rs
+++ tests/citeproc.rs
@@ -768,7 +768,7 @@ fn case_folding() {
         .content
         .write_buf(&mut buf, hayagriva::BufWriteFormat::Plain)
         .unwrap();
-    assert_eq!(buf, ". my lowercase container title");
+    assert_eq!(buf, ". my lowercase container title.");
 }
 
 #[test]
