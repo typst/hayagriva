ec8cdebe11446d58293f49bc56b827cfe3fc1fe2
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-02-01 23:14:53 +0800

    Fix delimiting within `<choose>` and `<layout>` for CSL (#269)
    * Fix delimiting within `<choose>` and `<layout>` for CSL
    
    Fixes #180
    
    A design trade-off:
    The delimiter stack uses `String` instead of `&str`.
    Using `&str` could eliminate many cloning, but at the cost of introducing complex lifetimes.
    Considering that delimiters are typically short, I choose `String`.
    
    * adjust comments and links
    
    * add nested group to ancestor delimiter test
    
    * clone strings directly
    
    * more accurate comments for delimiter push/pop
    
    ---------
    
    Co-authored-by: PgBiel <9021226+PgBiel@users.noreply.github.com>

--- src/csl/mod.rs
+++ src/csl/mod.rs
@@ -1674,6 +1674,9 @@ pub(crate) struct WritingContext {
     cases: NonEmptyStack<Option<TextCase>>,
     /// Inheritable name options.
     name_options: NonEmptyStack<InheritableNameOptions>,
+    /// Delimiters from ancestor delimiting elements (e.g., `cs:group`).
+    /// To be applied within `cs:choose`, but not another delimiting element or a macro.
+    delimiters: NonEmptyStack<Option<String>>,
 
     // Buffers.
     /// The buffer we're writing to. If block-level or formatting changes, we
@@ -1698,6 +1701,7 @@ impl Default for WritingContext {
             format_stack: NonEmptyStack::default(),
             cases: NonEmptyStack::default(),
             name_options: NonEmptyStack::default(),
+            delimiters: NonEmptyStack::default(),
             buf: CaseFolder::default(),
             elem_stack: NonEmptyStack::default(),
         }
@@ -1930,6 +1934,24 @@ impl WritingContext {
     fn reconfigure(&mut self) {
         self.buf
             .reconfigure((*self.cases.last()).map(Into::into).unwrap_or_default());
+    }
+
+    /// Set the delimiter of the children of a [`citationberg::Group`] or
+    /// [`citationberg::Layout`]. It is inherited by [`citationberg::Choose`]
+    /// for its children as well.
+    fn push_delimiter(&mut self, delimiter: Option<String>) -> DelimiterIdx {
+        let idx = self.delimiters.len();
+        self.delimiters.push(delimiter);
+        DelimiterIdx(idx)
+    }
+    /// Clear the delimiter after a [`citationberg::Group`] or
+    /// [`citationberg::Layout`].
+    fn pop_delimiter(&mut self, idx: DelimiterIdx) {
+        if idx.0 == self.delimiters.len() {
+            return;
+        }
+
+        self.delimiters.drain(idx.0).for_each(drop);
     }
 
     /// Push a new suppressed variable if we are suppressing queried variables.
@@ -2752,6 +2774,9 @@ impl DisplayLoc {
 
 #[must_use = "case stack must be popped"]
 struct CaseIdx(NonZeroUsize);
+
+#[must_use = "delimiter stack must be popped"]
+struct DelimiterIdx(NonZeroUsize);
 
 impl<T: EntryLike> Write for Context<'_, T> {
     fn write_str(&mut self, s: &str) -> std::fmt::Result {
